e2fbe67dbc78c68354f761b0732e355d54846a2d
weijun.liu <liuweijun@toki.waseda.jp>
2019-06-12 18:56:38 +0800

    fix some bug

--- config.sh
+++ config.sh
@@ -5,7 +5,6 @@ set -e
 sudo apt-get install zsh python-pip
 chsh -s /bin/zsh
 
-
 echo "====================="
 echo "1. Backup your config files. e.g. ~/.zshrc -> ~/.zshrc.backup"
 # Require Vim with Lua
@@ -20,27 +19,11 @@ fi
 echo "====================="
 echo "2. Install"
 
-sudo pip install flake8
-sudo mkdir -p ~/.vim_runtime
 sudo mkdir -p ~/.oh-my-zsh
 sudo mkdir -p ~/.config
-cp -R vim/* ~/.vim_runtime/
-# rsync -av --progress vim ~/.vim_runtime
-sh ~/.vim_runtime/install_awesome_vimrc.sh
 cp -R oh-my-zsh/* ~/.oh-my-zsh/
 chsh -s /bin/zsh
 cp oh-my-zsh/.zshrc ~/
-cp flake8 ~/.config/flake8
-
-# echo "====================="
-# echo "3. Config Vim"
-# git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim || true
-# pip install autopep8 --user
-# vim +PluginInstall +qall
-# git config --global diff.tool vimdiff
-# git config --global difftool.prompt false
-# git config --global alias.d difftool
-# (cd ~/.vim/bundle/YouCompleteMe && ./install.py)
 
 echo "====================="
 echo "Success!"
